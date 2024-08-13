f9c447f298e16f92ce2784d045b0eb8815317e1f
Анна <vindanka@gmail.com>
2024-08-13 21:36:36 +0300

    add transletion for simple ukrainian words

--- ua-en/TranslatorUkrainian.cs
+++ ua-en/TranslatorUkrainian.cs
@@ -2,5 +2,17 @@
 {
     internal static class TranslatorUkrainian
     {
+        internal static string TranslateUkrainian(string ukrainianWord)
+        {
+            ukrainianWord = ukrainianWord.ToLower();
+            switch (ukrainianWord)
+            {
+                case "привіт":
+                    return "hello";
+                case "бувай":
+                    return "bye";
+            }
+            return "i dunno how to translate it :(";
+        }
     }
 }
